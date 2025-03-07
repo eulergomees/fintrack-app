d24b5f7b927c5b73b397ebe4568e0c7feb9350df
Euler <eulergomesrocha@gmail.com>
2025-03-07 13:56:33 -0300

    expense detail

--- lib/pages/home/expensedetail_page.dart
+++ lib/pages/home/expensedetail_page.dart
@@ -3,13 +3,30 @@ import 'package:frontend/shared/models/expenses.dart';
 
 class ExpenseDetailPage extends StatelessWidget {
   final Expense expense;
-  ExpenseDetailPage({super.key, required this.expense});
+
+  const ExpenseDetailPage({super.key, required this.expense});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(expense.title),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(expense.type),
+                Text(expense.title),
+                Text("\$${expense.amount}"),
+                Text(expense.date),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
